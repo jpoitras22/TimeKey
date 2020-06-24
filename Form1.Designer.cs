2a6bb1d58177ff4e97e1fd0b33fc0206ec26fed6
jpoitras22 <36911364+jpoitras22@users.noreply.github.com>
2020-06-24 18:28:24 -0400

    Added timer length combobox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,15 +34,18 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_start
             // 
             this.btn_start.BackColor = System.Drawing.Color.Green;
             this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.28099F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_start.Location = new System.Drawing.Point(90, 73);
+            this.btn_start.Location = new System.Drawing.Point(99, 45);
             this.btn_start.Name = "btn_start";
-            this.btn_start.Size = new System.Drawing.Size(190, 109);
+            this.btn_start.Size = new System.Drawing.Size(163, 92);
             this.btn_start.TabIndex = 0;
             this.btn_start.Text = "Start";
             this.btn_start.UseVisualStyleBackColor = false;
@@ -52,9 +55,9 @@
             // 
             this.btn_stop.BackColor = System.Drawing.Color.Red;
             this.btn_stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.28099F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_stop.Location = new System.Drawing.Point(90, 236);
+            this.btn_stop.Location = new System.Drawing.Point(99, 176);
             this.btn_stop.Name = "btn_stop";
-            this.btn_stop.Size = new System.Drawing.Size(190, 109);
+            this.btn_stop.Size = new System.Drawing.Size(163, 89);
             this.btn_stop.TabIndex = 1;
             this.btn_stop.Text = "Stop";
             this.btn_stop.UseVisualStyleBackColor = false;
@@ -69,7 +72,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.90083F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(85, 375);
+            this.label1.Location = new System.Drawing.Point(83, 296);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 25);
             this.label1.TabIndex = 2;
@@ -80,17 +83,54 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.90083F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(170, 375);
+            this.label2.Location = new System.Drawing.Point(168, 296);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Stopped";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.90083F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(154, 343);
+            this.comboBox1.MaxDropDownItems = 5;
+            this.comboBox1.MaxLength = 1;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(55, 33);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.90083F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 346);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Time:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.90083F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(215, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Minutes";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(372, 423);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_stop);
@@ -111,6 +151,9 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,16 +12,28 @@ namespace TimeKey
 {
     public partial class Form1 : Form
     {
+        int timerLength = 240000;
+        
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Items.Add(1);
+            comboBox1.Items.Add(2);
+            comboBox1.Items.Add(3);
+            comboBox1.Items.Add(4);
+            comboBox1.Items.Add(5);
+            comboBox1.SelectedIndex = 3;
+
+            timer1.Interval = timerLength;
         }
 
         private void btn_start_Click(object sender, EventArgs e)
         {
             label2.Text = "Running";
             label2.ForeColor = Color.Green;
+            timer1.Interval = timerLength;
             timer1.Enabled = true;
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -34,6 +46,14 @@ namespace TimeKey
             label2.Text = "Stopped";
             label2.ForeColor = Color.Red;
             timer1.Enabled = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btn_stop_Click(sender, e);
+            timerLength = Convert.ToInt32(comboBox1.SelectedItem) * 60000;
+            
+            
         }
     }
 }
